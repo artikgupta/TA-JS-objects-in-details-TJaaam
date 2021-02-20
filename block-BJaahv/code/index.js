32576ac2361e5776eeffabdfebce92da5bde9d99
artikgupta <artig9262@gmail.com>
2021-02-20 11:49:57 +0530

    refactored

--- block-BJaahv/code/index.js
+++ block-BJaahv/code/index.js
@@ -1,24 +1,58 @@
-let projectMethods = {
-  getProjects: function () {
-    return this.noOfProjects;
-  },
-  changeName: function (newName) {
-    this.name = newName;
-  },
-  incrementProject: function () {
-    this.noOfProjects = noOfProjects + 1;
-  },
-  decrementProject: function () {
-    this.noOfProjects = noOfProjects - 1;
-  },
-};
-
 function createProject(name, id, noOfProjects) {
-  let projects = Object.create(projectMethods);
+  let projects = {};
   projects.name = name;
   projects.id = id;
   projects.noOfProjects = noOfProjects;
+  user.getProjects = function () {
+    return this.noOfProjects;
+  };
+  user.changeName = function (newName) {
+    let prevName = user.name;
+    user.name = prevName;
+    return prevName;
+  };
+  user.incrementProject = function () {
+    user.noOfProjects = noOfProjects + 1;
+    return user.noOfProjects;
+  };
+  user.decrementProject = function () {
+    user.noOfProjects = noOfProjects - 1;
+    return user.noOfProjects;
+  };
+  return user;
 }
+
+//Prototypal Pattern
+
+let userMethods = {
+  getProjects: function () {
+    return this.noOfProjects;
+  },
+  changeName = function (newName) {
+    let prevName = this.name;
+    this.name = prevName;
+    return prevName;
+  },
+  incrementProject = function () {
+  this.noOfProjects = noOfProjects + 1;
+    return this.noOfProjects;
+  },
+  decrementProject = function () {
+  this.noOfProjects = noOfProjects - 1;
+    return this.noOfProjects;
+  },
+};
+
+
+function createProject(name, id, noOfProjects) {
+  let user = Object.create(userMethods);
+  user.name = name;
+  user.id = id;
+  user.noOfProjects = noOfProjects;
+  
+}
+
+let user1 = createProject("Arti", 45, 193);
 
 // PsuedoClassical Pattern
 
@@ -33,8 +67,11 @@ CreateProject.prototype = {
     return this.noOfProjects;
   },
   changeName: function (newName) {
-    return (this.name = newName);
+    let prevName = this.name;
+    this.name = prevName;
+    return prevName;;
   },
+
   incrementProject: function () {
     return (this.noOfProjects = this.noOfProjects + 1);
   },
@@ -42,6 +79,8 @@ CreateProject.prototype = {
     return (this.noOfProjects = this.noOfProjects - 1);
   },
 };
+
+let user1 =  new createProject("Arti", 45, 193); 
 
 // Class
 
@@ -56,8 +95,10 @@ class Projects {
     return this.noOfProjects;
   }
 
-  changeName(newName) {
-    return (this.name = newName);
+   changeName (newName) {
+    let prevName = this.name;
+    this.name = prevName;
+    return prevName;;
   }
 
   incrementProject() {
