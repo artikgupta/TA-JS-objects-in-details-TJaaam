1ee702546b832a932bb4198eb5a322eba7e302c5
artikgupta <artig9262@gmail.com>
2021-02-24 21:59:29 +0530

    refactored

--- block-BJaaie/code/index.js
+++ block-BJaaie/code/index.js
@@ -63,3 +63,67 @@ Cat.prototype = {
     return `I am ${this.name} and the color of my eyes are ${this.colorOfEyes}. I can also do meow meow`;
   },
 };
+
+//Class
+
+class Animal {
+  constructor(location, numberOfLegs) {
+    this.location = location;
+    this.numberOfLegs = numberOfLegs;
+  }
+  eat() {
+    console.log(`I live in ${this.location} and I can eat`);
+  }
+
+  changeLocation(newLocation) {
+    this.location = newLocation;
+    return this.location;
+  }
+
+  summary() {
+    return `I live in ${this.location} and I have ${this.numberOfLegs}`;
+  }
+}
+
+class Dog extends Animal {
+
+ super(location, numberOfLegs)
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  changeName (newName) {
+    this.name = newName;
+    return this.name;
+  }
+  changeColor (newColor) {
+    this.color = newColor;
+    return this.color;
+  }
+  summary() {
+    return `I am ${this.name} and I am of ${this.color} color. I can also bark`;
+  }
+}
+
+ class Cat {
+    constructor(name, colorOfEyes){
+    super(location, numberOfLegs)
+    this.name= name;
+    this.colorOfEyes= colorOfEyes;
+    }
+    meow() {
+        alert(`I am ${this.name} and I can do mewo meow 😹`);
+      }
+      changeName (newName) {
+        this.name = newName;
+        return this.name;
+      }
+      changeColorOfEyes (newColor) {
+        this.colorOfEyes = newColor;
+        return this.colorOfEyes;
+      }
+      summary() {
+        return `I am ${this.name} and the color of my eyes are ${this.colorOfEyes}. I can also do meow meow`;
+      }
+}
